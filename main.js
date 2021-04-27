8c06fcc106f99b239ad5d12e3fb2a8feb0693eb5
samantha <lattechoi2@gmail.com>
2021-04-27 11:02:49 +0900

    js 버튼 이벤트 추가

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const moreBtn = document.querySelector('.info .video__metadata .titleAndBtn .moreBtn');
+const title = document.querySelector('.info .video__metadata .titleAndBtn .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+})
